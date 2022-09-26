03cf6ee2ad8d08671b1ab8074d5f966e65b120ea
waldsteiner <waldsteiner@gmail.com>
2022-09-27 02:46:09 +0900

    변경사항   - 코스 수정 중

--- server/BocamDogam/src/main/java/com/codestates/BocamDogam/course/entity/Course.java
+++ server/BocamDogam/src/main/java/com/codestates/BocamDogam/course/entity/Course.java
@@ -17,18 +17,41 @@ public class Course{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long courseId;
+
+    @Column
     private String name;
+
+    @Column
+    private String description;
+
+    @Column
     private String category;
+
+    @Column
     private String applyStart;
+
+    @Column
     private String applyEnd;
+
+    @Column
     private String StartDate;
+
+    @Column
     private String EndDate;
+
+    @Column
     private int period;
+    @Column
     private int scale;
+    @Column
     private int cost;
+    @Column
     private boolean support;
+    @Column
     private boolean remote;
-    private boolean isApplying;
+    @Column
+    private String ApplyStatus;
+    // 이넘 타입으로 변경
 
     @ManyToOne
     @JoinColumn(name = "institute_id")
