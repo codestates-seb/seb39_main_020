a2ab6f4a23162f9cabdd39cd8e9d18b7616e70f8
leesukkyeong <104298444+leesukkyeong@users.noreply.github.com>
2022-09-26 17:25:46 +0900

    initial commit

---
+++ seb39_main_020 3/server/BocamDogam/src/main/java/com/codestates/BocamDogam/BocamDogamApplication.java
@@ -0,0 +1,12 @@
+package com.codestates.BocamDogam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BocamDogamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BocamDogamApplication.class, args);
+	}
+}
