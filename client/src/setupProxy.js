b15e737c5202d6e0136963b374964971e73104c3
김경민 <gimgyeongmin@gimgyeongmin-ui-MacBookAir.local>
2022-09-26 15:17:14 +0900

    죄송해요 다시바꿨어요 ㅜㅜ

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/main',
+    '/',
     //개인 테스트 할 시 포트3000, 합동 테스트 할 시 포트8080
     createProxyMiddleware({
       target: 'http://localhost:8080',
