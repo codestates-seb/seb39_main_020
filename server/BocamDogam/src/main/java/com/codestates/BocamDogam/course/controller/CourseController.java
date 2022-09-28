e734b61428a8ee95495f8d4fc2f161e15c4f27af
waldsteiner <waldsteiner@gmail.com>
2022-09-29 04:00:39 +0900

    변경사항   - Course 부분 작업 내용 추가

--- server/BocamDogam/src/main/java/com/codestates/BocamDogam/course/controller/CourseController.java
+++ server/BocamDogam/src/main/java/com/codestates/BocamDogam/course/controller/CourseController.java
@@ -1,20 +1,58 @@
 package com.codestates.BocamDogam.course.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codestates.BocamDogam.course.mapper.CourseMapper;
+import com.codestates.BocamDogam.course.service.CourseService;
+import com.codestates.BocamDogam.institute.service.InstituteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/main/institutes/{institute-id}")
+@RequestMapping("/main/institutes/{institute-id}/courses")
 public class CourseController {
+    private final CourseService courseService;
+    private final CourseMapper courseMapper;
+    private final InstituteService instituteService;
 
+    public CourseController(CourseService courseService,
+                            CourseMapper courseMapper,
+                            InstituteService instituteService) {
+        this.courseService = courseService;
+        this.courseMapper = courseMapper;
+        this.instituteService = instituteService;
+    }
 
     // 특정 교육기관의 교육과정 생성 요청
+    @PostMapping
+    public ResponseEntity postCourse() {
+
+        return new ResponseEntity<>();
+    }
 
     // 특정 교육기관의 교육과정 조회 요청
+    @GetMapping("/main/institutes/{institute-id}/courses/{course-id}")
+    public ResponseEntity getCourse() {
+
+        return new ResponseEntity<>();
+    }
 
     // 해당 교육기관의 모든 교육 과정 조회 요청
+    @GetMapping
+    public ResponseEntity getCourses() {
+
+        return new ResponseEntity<>();
+    }
 
     // 특정 교육기관의 교육과정 정보 수정 요청
+    @PatchMapping("/main/institutes/{institute-id}/courses/{course-id}")
+    public ResponseEntity getCourses() {
+
+        return new ResponseEntity<>();
+    }
 
     // 특정 교육기관의 교육과정 삭제 요청
+    @DeleteMapping("/main/institutes/{institute-id}/courses/{course-id}")
+    public ResponseEntity deleteCourses() {
+
+
+    }
 }
--- server/BocamDogam/src/main/java/com/codestates/BocamDogam/course/service/CourseServiceImpl.java
+++ server/BocamDogam/src/main/java/com/codestates/BocamDogam/course/service/CourseServiceImpl.java
@@ -26,15 +26,16 @@ public class CourseServiceImpl implements CourseService {
     // 교육과정 생성
     @Override
     public Course createCourse(Course course) {
-        // verifiedCourse(course);
-        // Course savedCourse = saveCourse(course);
+        verifyCourse(course);
+        Course savedCourse = courseRepository.save(course);
 
-        return null; // savedCourse;
+        return savedCourse;
     }
 
     // 교육과정 조회
     @Override
     public Course findCourse(Long courseId) {
+
         return findVerifiedCourse(courseId);
     }
 
@@ -42,13 +43,43 @@ public class CourseServiceImpl implements CourseService {
     // 페이지네이션 필요한가?
     @Override
     public List<Course> findAllCourse() {
+
         return courseRepository.findAll();
     }
 
     // 교육과정 정보 수정
     @Override
     public Course updateCourse(Course course) {
-        return null;
+        Course findCourse = findVerifiedCourse(course.getCourseId());
+        Optional.of(course.getName())
+                .ifPresent(name -> findCourse.setName(name));
+        Optional.of(course.getDescription())
+                .ifPresent(description -> findCourse.setDescription(description));
+        Optional.of(course.getCategory())
+                .ifPresent(category -> findCourse.setCategory(category));
+        Optional.of(course.getLecturer())
+                .ifPresent(lecturer -> findCourse.setLecturer(lecturer));
+        Optional.of(course.isSupport())
+                .ifPresent(support -> findCourse.setSupport(support));
+        Optional.of(course.isRemote())
+                .ifPresent(remote -> findCourse.setRemote(remote));
+        Optional.of(course.getScale())
+                .ifPresent(scale -> findCourse.setScale(scale));
+        Optional.of(course.getCost())
+                .ifPresent(cost -> findCourse.setCost(cost));
+        Optional.of(course.getStartDate())
+                .ifPresent(startDate -> findCourse.setStartDate(startDate));
+        Optional.of(course.getEndDate())
+                .ifPresent(endDate -> findCourse.setEndDate(endDate));
+        Optional.of(course.getApplyStart())
+                .ifPresent(applyStart -> findCourse.setApplyStart(applyStart));
+        Optional.of(course.getApplyEnd())
+                .ifPresent(applyEnd -> findCourse.setApplyEnd(applyEnd));
+
+        // 기간 체크 필요
+        // 모집 상태 체크 필요
+
+        return courseRepository.save(findCourse);
     }
 
     // 교육과정 제거
@@ -61,7 +92,7 @@ public class CourseServiceImpl implements CourseService {
     // 코스 등록 전 검증
     @Override
     public void verifyCourse(Course course) {
-
+        instituteService.findVerifiedInstitute(course.getInstitute().getInstituteId());
     }
 
     // 등록된 코스
@@ -85,14 +116,20 @@ public class CourseServiceImpl implements CourseService {
 
     // 현재 모집 중인지 확인
     public LocalDateTime isApplying() {
-        // 저장된 종료일, 시작일 불러오기
-        // 페리오드 비트윈 함수를 사용하여 두 사이에 시간 구하기
-        // 일로만 표시 =>
+        // 현재일 > 모집마감일 = 모집 종료
+        // 현재일 <= 모집마감일 && 일수 차이 < 3 = 마감 임박
+        // 현재일 <= 모집마감일 = 모집 중
+        // 등록일, 마감일 없을 = 등록 예정
+
         return null;
     }
 
     // 전체 수업 일수 계산
     public LocalDateTime coursePeriod() {
+        // 저장된 종료일, 시작일 불러오기
+        // 페리오드 비트윈 함수를 사용하여 두 사이에 시간 구하기
+        // 일로만 표시 =>
+
         return null;
     }
 
